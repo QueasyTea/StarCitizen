aa50049f7207434028c223aac82bd5fb480c4360
Iaroslav <jaroslav691@gmail.com>
2020-12-09 08:02:51 +0300

    HW2. Реализовал движение "front.jpg" при нажатии клавиши мыши (touchDown) в точку нажатия на экране и остановку в данной точке.

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,7 +1,6 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
@@ -11,8 +10,13 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 0.5f;
+
     private Texture img;
     private Vector2 pos;
+    private Vector2 touch;
+    private Vector2 v;
+    private Vector2 tmp;
 
 
 
@@ -21,11 +25,20 @@ public class MenuScreen extends BaseScreen {
         super.show();
         img = new Texture("front.jpg");
         pos = new Vector2();
+        touch = new Vector2();
+        v = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        tmp.set(touch);
+        if(touch.cpy().sub(pos).len() > v.len()){
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
         Gdx.gl.glClearColor(0.5f, 0.23f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
@@ -42,29 +55,12 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
 
-    @Override
-    public boolean keyDown(int keycode) {
-        switch (keycode){
-            case Input.Keys.UP : pos.y += 10;
-            break;
-            case  Input.Keys.DOWN : pos.y -= 10;
-            break;
-            case Input.Keys.RIGHT : pos.x += 10;
-            break;
-            case Input.Keys.LEFT : pos.x -= 10;
-            break;
 
-        }
-        return super.keyDown(keycode);
-    }
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
 
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDragged(screenX, screenY, pointer);
+
+        return false;
     }
 }
